5c6da6aa37c4b46b4f268265b41fe736b69679d4
Hannah Santos <hannah.santos.441@ufrn.edu.br>
2023-09-19 12:29:10 -0300

    Added forall_as_neg_exists_law theorem contaminated ⛤

--- src/logic.lean
+++ src/logic.lean
@@ -453,10 +453,10 @@ theorem exists_as_neg_forall_converse :
   ¬(∀x, ¬P x) → (∃x, P x)  := by sorry
 
 theorem forall_as_neg_exists_law :
-  (∀x, P x) ↔ ¬(∃x, ¬P x)  :=
-begin
-  sorry,
-end
+  (∀x, P x) ↔ ¬(∃x, ¬P x)  := by
+  apply Iff.intro
+  exact forall_as_neg_exists U P
+  exact forall_as_neg_exists_converse U P
 
 theorem exists_as_neg_forall_law :
   (∃x, P x) ↔ ¬(∀x, ¬P x)  :=
